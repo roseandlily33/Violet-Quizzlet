1077cf869656c1b48f7c2c48345dc12de1b6a810
Victoria Benoit <vrose834@gmail.com>
2023-02-07 14:28:58 -0400

    Finished Project

--- script2.js
+++ script2.js
@@ -1,3 +1,4 @@
+//My API Selectors:
 let startBtn = document.getElementById('start-btn');
 let questionCont = document.getElementById('main-questions');
 let beginningEl = document.getElementById('intro');
@@ -8,7 +9,10 @@ let finalEl = document.getElementById('final');
 let scorePage = document.getElementById('score-page');
 let submitBtn = document.getElementById('submitInit');
 let scoresCont = document.getElementById('scores');
+let startOver = document.getElementById('back');
+let clearScores = document.getElementById('clear');
 
+//My Questions: 
 const questions = [
     {
         question: 'What direction does align-items align on if your aligning on the horizontal axis?',
@@ -31,18 +35,20 @@ const questions = [
         correct: 'All of the above'
     }
 ];
-
+//Variables:
 let currentQuestionIndex = 0;
 
 let finalQuestion = questions.length;
 let seconds = 75;
 let highScores = JSON.parse(localStorage.getItem('score')) || [];
 let initials = document.querySelector('#inputInit');
-
+let score = 0;
 var timerInterval;
 
+//Click to start the game:
 startBtn.addEventListener('click', startGame);
 
+//Start Game function:
 function startGame() {
     startBtn.classList.add('hide');
     beginningEl.classList.add('hide');
@@ -50,30 +56,33 @@ function startGame() {
     timer();
     nextQuestion();
 }
+//Timer function:
 function timer() {
      timerInterval = setInterval(function () {
         seconds--;
         timerEl.textContent = 'Time left: ' + seconds;
         if (seconds === 0) {
             clearInterval(timerInterval);
-            scorePage();
+            finalScore();
         }
     }, 1000
     );
 };
+
+//Goes through the questions:
 function nextQuestion() {
-    if(currentQuestionIndex == finalQuestion){
+    if(currentQuestionIndex === finalQuestion){
         finalScore();
         return;
-    } else if(seconds == 0){
+    } else if(seconds === 0){
+        clearInterval(timerInterval);
         finalScore();
-    } else{
     }
-    buttonCont.innerHTML = " ";
+     else{
+    
+    buttonCont.innerHTML = "";
     let currentQuestion = questions[currentQuestionIndex];
     questionEl.textContent = currentQuestion.question;
-    //for (let i = 0; i < questions.length; i++) {
-    //currentQuestionIndex.textContent = currentQuestion.question;}
 
     currentQuestion.choices.forEach(answer =>{
     let button = document.createElement('button');
@@ -81,45 +90,58 @@ function nextQuestion() {
     buttonCont.appendChild(button);
     
     button.addEventListener('click',function(){
-        if(answer === currentQuestion.correct){
-            this.style.backgroundColor = 'green';
+        if(answer == currentQuestion.correct){
+           seconds;
         } else {
-            this.style.backgroundColor = 'red';
-            seconds -= 10;
+          seconds -= 10 ;
         }
         currentQuestionIndex++;
      nextQuestion();
-    })})
-    
-   }
-
+    })});}}
+  
+    //Shows the final score and stops the timer:
    function finalScore(){
     questionCont.classList.add('hide');
     finalEl.classList.remove('hide');
     clearInterval(timerInterval);
   
    }
-
+    //JSON parses back through to show the scores on the leaderboard:
    function scores(){
     finalEl.classList.add('hide');
     scorePage.classList.remove('hide');
-    scoresCont.innerHtml = highScores;
-
-
-
     var returnedScore = JSON.parse(localStorage.getItem('score'));
     if(returnedScore !== null){
-        document.getElementById.innerHTML = returnedScore.score;
-    } else { return;}
+        returnedScore.forEach(person => {
+        let name = document.createElement('h4');
+        name.textContent = person.initials + '  -  ' + person.score;
+        scoresCont.appendChild(name);
+        })
+    } else { return;} 
    } 
+
+//Submits the score to local storage with name:
    submitBtn.addEventListener('click', function(e){
     e.preventDefault();
-   console.log(initials.value);
+   //console.log(initials.value);
     var data = {
         initials: initials.value,
         score: seconds
     }
+    //Pushes data to local storage:
     highScores.push(data);
     localStorage.setItem('score',JSON.stringify(highScores));
     scores();
 })
+   //Starts the quiz over:
+   startOver.addEventListener('click', function(){
+    window.location.href = "index.html";
+   });
+
+    //Clears the scores when pressed:
+   clearScores.addEventListener('click', function(){
+    //highScores = [];
+    localStorage.clear('returnedScore');
+    scoresCont.innerHTML = "";
+   })
+  
